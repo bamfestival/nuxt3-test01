4842624bb64553f527a71bfefa11c5aeb804709a
rps2305 <73200332+rps2305@users.noreply.github.com>
2023-01-15 22:18:57 +0100

    security module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   mode: "universal",
-    modules: ['nuxt-icon'],
+    modules: ['nuxt-icon','nuxt-security'],
     css: ['~/assets/css/main.css'],
     postcss: {
       plugins: {
